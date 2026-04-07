5fe005131568162a1520e24d3c0844bab3dcd75b
David <dorsiwebdesign@gmail.com>
2026-04-07 02:43:55 -0300

    Create top-selling-product-overall.sql

---
+++ top-selling-product-overall.sql
@@ -0,0 +1,17 @@
+-- Top-selling product overall
+
+SELECT 
+    p.ProductName,
+    SUM(od.Quantity) AS TotalSold
+
+FROM OrderDetails od
+JOIN Products p 
+     ON od.ProductID = p.ProductID
+
+GROUP BY 
+    p.ProductName
+
+ORDER BY 
+    TotalSold DESC
+
+LIMIT 1;
